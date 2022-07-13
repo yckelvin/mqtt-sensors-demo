4f42b85da07bc4ab246706180fc993634564f4f2
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-07-13 10:40:49 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    microIoT.microIoT_clear()
+    data = pins.analogReadPin(AnalogPin.P0)
+    publish(0, convertToText(data))
+})
+function publish (num: number, text: string) {
+    microIoT.microIoT_SendMessage(text, microIoT.TOPIC.topic_0)
+    microIoT.microIoT_showUserText(num, "Send... " + text)
+}
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
+})
+let data = 0
+let wifi_name = "izowifi"
+let password = "izo1234@"
+let iot_id = "lmZB9bXGR"
+let iot_pwd = "liWfrxXMgz"
+let topic_0 = "qwPmNL37g"
+microIoT.microIoT_initDisplay()
+microIoT.microIoT_WIFI(wifi_name, password)
+microIoT.microIoT_MQTT(
+iot_id,
+iot_pwd,
+topic_0,
+microIoT.SERVERS.English
+)
