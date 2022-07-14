db8a7c480b85faa381b8ad9555e4795fa894112d
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-07-14 15:10:29 +0800

    add touch sensor

--- main.ts
+++ main.ts
@@ -1,16 +1,7 @@
-input.onButtonPressed(Button.A, function () {
-    microIoT.microIoT_clear()
-    data = pins.analogReadPin(AnalogPin.P0)
-    publish(0, convertToText(data))
-})
 function publish (num: number, text: string) {
     microIoT.microIoT_SendMessage(text, microIoT.TOPIC.topic_0)
     microIoT.microIoT_showUserText(num, "Send... " + text)
 }
-input.onButtonPressed(Button.AB, function () {
-    control.reset()
-})
-let data = 0
 let wifi_name = "izowifi"
 let password = "izo1234@"
 let iot_id = "lmZB9bXGR"
@@ -24,3 +15,10 @@ iot_pwd,
 topic_0,
 microIoT.SERVERS.English
 )
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.clearScreen()
+    }
+})
