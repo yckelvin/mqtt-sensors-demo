b396d32c3abb55e2ced23b21ed286cc9c9288f05
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-08-01 22:27:33 +0800

    add debug

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     speed += -10
     microIoT.microIoT_MotorRun(microIoT.aMotors.M1, microIoT.Dir.CW, speed)
-    microIoT.microIoT_clear()
     publish(0, convertToText(speed))
 })
 function publish (num: number, text: string) {
     microIoT.microIoT_SendMessage(text, microIoT.TOPIC.topic_0)
+    microIoT.microIoT_clear()
     microIoT.microIoT_showUserText(num, "Send... " + text)
 }
 input.onButtonPressed(Button.AB, function () {
@@ -14,21 +14,24 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     speed += 10
     microIoT.microIoT_MotorRun(microIoT.aMotors.M1, microIoT.Dir.CW, speed)
-    microIoT.microIoT_clear()
     publish(0, convertToText(speed))
 })
 let speed = 0
+basic.showNumber(0)
 speed = 0
 let wifi_name = "izowifi"
 let password = "izo1234@"
 let iot_id = "lmZB9bXGR"
 let iot_pwd = "liWfrxXMgz"
 let topic_0 = "qwPmNL37g"
+basic.showNumber(0)
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_WIFI(wifi_name, password)
+basic.showNumber(0)
 microIoT.microIoT_MQTT(
 iot_id,
 iot_pwd,
 topic_0,
 microIoT.SERVERS.English
 )
+basic.showNumber(0)
