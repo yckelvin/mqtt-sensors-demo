983fb82e46105f77a88f795df75a3703333eca4b
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-08-01 22:11:55 +0800

    add debug

--- main.ts
+++ main.ts
@@ -1,22 +1,26 @@
 let lightintensity = 0
+basic.showNumber(0)
 let wifi_name = "izowifi"
 let password = "izo1234@"
 let iot_id = "lmZB9bXGR"
 let iot_pwd = "liWfrxXMgz"
 let topic_0 = "qwPmNL37g"
+basic.showNumber(1)
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_WIFI(wifi_name, password)
+basic.showNumber(2)
 microIoT.microIoT_MQTT(
 iot_id,
 iot_pwd,
 topic_0,
 microIoT.SERVERS.English
 )
+basic.showNumber(3)
 basic.forever(function () {
     lightintensity = 1023 - pins.analogReadPin(AnalogPin.P0)
     microIoT.microIoT_showUserText(0, "Light: " + convertToText(lightintensity))
-    microIoT.microIoT_SendMessage(convertToText(lightintensity), microIoT.TOPIC.topic_0)
     if (lightintensity < 500) {
+        pins.analogWritePin(AnalogPin.P1, 1023)
         basic.showLeds(`
             # # # # #
             # # # # #
