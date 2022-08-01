91f1c4020902b57fe74cb81c805cdb7c77982ee8
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-08-01 22:19:23 +0800

    add debug

--- main.ts
+++ main.ts
@@ -6,29 +6,33 @@ input.onButtonPressed(Button.A, function () {
 })
 function publish (num: number, text: string) {
     microIoT.microIoT_SendMessage(text, microIoT.TOPIC.topic_0)
+    microIoT.microIoT_clear()
     microIoT.microIoT_showUserText(num, "Send... " + text)
 }
 input.onButtonPressed(Button.AB, function () {
     control.reset()
 })
 input.onButtonPressed(Button.B, function () {
-    microIoT.microIoT_clear()
     light_level += 50
     pins.analogWritePin(AnalogPin.P0, light_level)
     publish(0, convertToText(light_level))
 })
 let light_level = 0
+basic.showNumber(0)
 light_level = 0
 let wifi_name = "izowifi"
 let password = "izo1234@"
 let iot_id = "lmZB9bXGR"
 let iot_pwd = "liWfrxXMgz"
 let topic_0 = "qwPmNL37g"
+basic.showNumber(1)
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_WIFI(wifi_name, password)
+basic.showNumber(2)
 microIoT.microIoT_MQTT(
 iot_id,
 iot_pwd,
 topic_0,
 microIoT.SERVERS.English
 )
+basic.showNumber(3)
