8a0d0fb22ce4b60bfe2f96c2b4b6ac6da2611480
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-08-01 22:23:01 +0800

    add debug

--- main.ts
+++ main.ts
@@ -1,20 +1,25 @@
 function publish (num: number, text: string) {
     microIoT.microIoT_SendMessage(text, microIoT.TOPIC.topic_0)
+    microIoT.microIoT_clear()
     microIoT.microIoT_showUserText(num, "Send... " + text)
 }
+basic.showNumber(0)
 let wifi_name = "izowifi"
 let password = "izo1234@"
 let iot_id = "lmZB9bXGR"
 let iot_pwd = "liWfrxXMgz"
 let topic_0 = "qwPmNL37g"
+basic.showNumber(0)
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_WIFI(wifi_name, password)
+basic.showNumber(0)
 microIoT.microIoT_MQTT(
 iot_id,
 iot_pwd,
 topic_0,
 microIoT.SERVERS.English
 )
+basic.showNumber(0)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
         basic.showIcon(IconNames.Heart)
