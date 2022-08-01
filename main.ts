64c39cbd7123677081b5867993bca91015fd3e16
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-08-01 22:26:19 +0800

    add debug

--- main.ts
+++ main.ts
@@ -1,21 +1,26 @@
 function publish (num: number, text: string) {
     microIoT.microIoT_SendMessage(text, microIoT.TOPIC.topic_0)
+    microIoT.microIoT_clear()
     microIoT.microIoT_showUserText(num, "Send... " + text)
 }
 let data = 0
+basic.showNumber(0)
 let wifi_name = "izowifi"
 let password = "izo1234@"
 let iot_id = "lmZB9bXGR"
 let iot_pwd = "liWfrxXMgz"
 let topic_0 = "qwPmNL37g"
+basic.showNumber(1)
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_WIFI(wifi_name, password)
+basic.showNumber(2)
 microIoT.microIoT_MQTT(
 iot_id,
 iot_pwd,
 topic_0,
 microIoT.SERVERS.English
 )
+basic.showNumber(3)
 basic.forever(function () {
     data = pins.analogReadPin(AnalogPin.P0)
     led.plotBarGraph(
