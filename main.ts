62959c1256f06c278b15d5880eca84ad6e25e2bc
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-08-01 22:10:45 +0800

    add debug

--- main.ts
+++ main.ts
@@ -8,16 +8,20 @@ input.onButtonPressed(Button.AB, function () {
     control.reset()
 })
 let lightintensity = ""
+basic.showNumber(0)
 let wifi_name = "izowifi"
 let password = "izo1234@"
 let iot_id = "lmZB9bXGR"
 let iot_pwd = "liWfrxXMgz"
 let topic_0 = "qwPmNL37g"
+basic.showNumber(1)
 microIoT.microIoT_initDisplay()
 microIoT.microIoT_WIFI(wifi_name, password)
+basic.showNumber(2)
 microIoT.microIoT_MQTT(
 iot_id,
 iot_pwd,
 topic_0,
 microIoT.SERVERS.English
 )
+basic.showNumber(3)
